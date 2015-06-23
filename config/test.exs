5498d150001564895365e4326d0bee7a8d737d82
Richard Bishop <richard@rubiquity.com>
2015-06-22 21:22:37 -0600

    Add missing comma after endpoint config

--- config/test.exs
+++ config/test.exs
@@ -5,6 +5,6 @@ hostname = List.to_string hostname
 
 config :honeybadger,
   api_key: "at3stk3y",
-  endpoint: "https://api.honeybadger.io"
+  endpoint: "https://api.honeybadger.io",
   hostname: hostname,
   project_root: System.cwd
