29aaeeede4dc58fea70b0dcefe1143d5fc32405a
Richard Bishop <richard@rubiquity.com>
2015-06-22 21:22:06 -0600

    Add tests for converting a stacktrace to a backtrace

---
+++ test/backtrace_test.exs
@@ -0,0 +1,14 @@
+defmodule Honeybadger.BacktraceTest do
+  alias Honeybadger.Backtrace
+  use ExUnit.Case
+  
+  test "converting a stacktrace to the format Honeybadger expects" do
+    stacktrace = [{Honeybadger, :notify, [], [file: 'lib/honeybadger.ex', line: 38]},
+                  {Honeybadger.Backtrace, :from_stacktrace, [], [file: 'lib/honeybadger/backtrace.ex', line: 4]}]
+
+    backtrace = Backtrace.from_stacktrace stacktrace
+
+    assert [%{file: "lib/honeybadger.ex", line: 38, method: "notify"},
+            %{file: "lib/honeybadger/backtrace.ex", line: 4, method: "from_stacktrace"}] == backtrace
+  end
+end
