08c7311d50e89b6e3e91ba28840d4792838b54f6
Richard Bishop <richard@rubiquity.com>
2015-06-22 22:43:18 -0600

    Users can send a notice to the collector API

--- lib/honeybadger.ex
+++ lib/honeybadger.ex
@@ -1,2 +1,18 @@
 defmodule Honeybadger do
+  alias Honeybadger.Backtrace
+  alias Honeybadger.Notice
+  alias HTTPoison, as: HTTP
+  alias Poison, as: JSON
+
+  @api_url Application.get_env(:honeybadger, :endpoint) <> "/v1/notices"
+  @api_key Application.get_env(:honeybadger, :api_key)
+  @headers [{"Accept", "application/json"},
+            {"Content-Type", "application/json"},
+            {"X-API-Key", @api_key}]
+
+  def notify(exception, metadata \\ %{}) do
+    backtrace = Backtrace.from_stacktrace System.stacktrace
+    {:ok, body} = JSON.encode Notice.new(exception, backtrace, metadata)
+    HTTP.post @api_url, body, @headers
+  end
 end
--- test/honeybadger_test.exs
+++ test/honeybadger_test.exs
@@ -1,7 +1,18 @@
 defmodule HoneybadgerTest do
   use ExUnit.Case
+  alias HTTPoison, as: HTTP
+  import Mock
 
-  test "the truth" do
-    assert 1 + 1 == 2
-  end
+  test "sending a notice" do
+    with_mock HTTP, [post: fn(_url, _data, _headers) -> %HTTP.Response{} end] do
+      exception = %RuntimeError{message: "Oops"}
+      url = Application.get_env(:honeybadger, :endpoint) <> "/v1/notices"
+      headers = [{"Accept", "application/json"},
+                 {"Content-Type", "application/json"},
+                 {"X-API-Key", "at3stk3y"}]
+
+      Honeybadger.notify exception
+
+      assert called HTTP.post(url, body, headers)
+    end
 end
