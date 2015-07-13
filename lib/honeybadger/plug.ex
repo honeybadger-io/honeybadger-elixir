f80379417a613d1d858a7a411a1a9fdf77cb048f
Richard Bishop <richard@rubiquity.com>
2015-07-12 21:13:02 -0600

    Move all functions except handle_errors/2 outside of __using__

--- lib/honeybadger/plug.ex
+++ lib/honeybadger/plug.ex
@@ -1,7 +1,7 @@
 defmodule Honeybadger.Plug do
   defmacro __using__(_env) do
     quote do
-      import Plug.Conn
+      import Honeybadger.Plug
       use Plug.ErrorHandler
 
       # Exceptions raised on non-existant routes are ignored
@@ -33,40 +33,40 @@ defmodule Honeybadger.Plug do
 
         Honeybadger.notify reason, %{plug_env: metadata}, stack
       end
-
-      defp get_component_from_module do
-        __MODULE__ 
-        |> Atom.to_string
-        |> String.split(".") 
-        |> List.last
-      end
-
-      defp build_cgi_data(%Plug.Conn{} = conn) do
-        rack_env_http_vars = Enum.reduce conn.req_headers, %{}, &header_to_rack_format/2
-        cgi_data = %{
-          "REQUEST_METHOD" => conn.method,
-          "PATH_INFO" => Enum.join(conn.path_info, "/"),
-          "QUERY_STRING" => conn.query_string,
-          "SCRIPT_NAME" => Enum.join(conn.script_name, "/"),
-          "REMOTE_ADDR" => get_remote_addr(conn.remote_ip),
-          "REMOTE_PORT" => get_remote_port(conn.peer),
-          "SERVER_ADDR" => "127.0.0.1",
-          "SERVER_NAME" => Application.get_env(:honeybadger, :hostname),
-          "SERVER_PORT" => conn.port,
-          "CONTENT_LENGTH" => get_req_header(conn, "content-length"),
-          "ORIGINAL_FULLPATH" => full_path(conn)
-        } 
-
-        Map.merge rack_env_http_vars, cgi_data
-      end
-
-      defp get_remote_addr(addr), do: :inet.ntoa(addr) |> List.to_string
-      defp get_remote_port({_, port}), do: port
-
-      defp header_to_rack_format({header, value}, acc) do
-        header = "HTTP_" <> String.upcase(header) |> String.replace("-", "_")
-        Map.put acc, header, value
-      end
     end
+  end
+
+  def get_component_from_module do
+    __MODULE__ 
+    |> Atom.to_string
+    |> String.split(".") 
+    |> List.last
+  end
+
+  def build_cgi_data(%Plug.Conn{} = conn) do
+    rack_env_http_vars = Enum.reduce conn.req_headers, %{}, &header_to_rack_format/2
+    cgi_data = %{
+      "REQUEST_METHOD" => conn.method,
+      "PATH_INFO" => Enum.join(conn.path_info, "/"),
+      "QUERY_STRING" => conn.query_string,
+      "SCRIPT_NAME" => Enum.join(conn.script_name, "/"),
+      "REMOTE_ADDR" => get_remote_addr(conn.remote_ip),
+      "REMOTE_PORT" => get_remote_port(conn.peer),
+      "SERVER_ADDR" => "127.0.0.1",
+      "SERVER_NAME" => Application.get_env(:honeybadger, :hostname),
+      "SERVER_PORT" => conn.port,
+      "CONTENT_LENGTH" => Plug.Conn.get_req_header(conn, "content-length"),
+      "ORIGINAL_FULLPATH" => Plug.Conn.full_path(conn)
+    } 
+
+    Map.merge rack_env_http_vars, cgi_data
+  end
+
+  def get_remote_addr(addr), do: :inet.ntoa(addr) |> List.to_string
+  def get_remote_port({_, port}), do: port
+
+  def header_to_rack_format({header, value}, acc) do
+    header = "HTTP_" <> String.upcase(header) |> String.replace("-", "_")
+    Map.put acc, header, value
   end
 end
