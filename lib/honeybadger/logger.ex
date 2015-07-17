19b47ee75cd72ae96f7f9681bf20035b369263c5
Richard Bishop <richard@rubiquity.com>
2015-07-16 22:35:33 -0600

    Logger state is always empty, context is just whatever is in the pdict minus the pid

--- lib/honeybadger/logger.ex
+++ lib/honeybadger/logger.ex
@@ -2,51 +2,46 @@ defmodule Honeybadger.Logger do
   require Logger
 
   @exception_format ~r/\((?<exception>.*?)\) (?<message>(.*))/
+  @ignored_keys [:pid]
 
   use GenEvent
 
-  def init(_module) do
-    context_keys = Application.get_env(:honeybadger, :context, [])
-    {:ok, context_keys}
-  end
+  def init(_mod), do: {:ok, []}
 
-  def handle_call({:configure, new_keys}, _context_keys) do
+  def handle_call({:configure, new_keys}, _state) do
     {:ok, :ok, new_keys}
   end
 
-  def handle_event({level, _gl, _event}, context_keys)
+  def handle_event({level, _gl, _event}, state)
   when level != :error do
-    {:ok, context_keys}
+    {:ok, state}
   end
 
-  def handle_event({_level, gl, _event}, context_keys)
+  def handle_event({_level, gl, _event}, state)
   when node(gl) != node() do
-    {:ok, context_keys}
+    {:ok, state}
   end
 
   # Error messages from Ranch/Cowboy come in the form of iodata. We ignore
   # these because they should already be reported by Honeybadger.Plug.
-  def handle_event({:error, _gl, {_mod, message, _ts, _pdict}}, context_keys) 
+  def handle_event({:error, _gl, {_mod, message, _ts, _pdict}}, state) 
   when is_list(message) do
-    {:ok, context_keys}
+    {:ok, state}
   end
 
-  def handle_event({:error, _gl, {Logger, message, _ts, pdict}}, context_keys) do
+  def handle_event({:error, _gl, {Logger, message, _ts, pdict}}, state) do
     try do
-      exception = exception_from_message message
-      context = Dict.take(pdict, context_keys)
-      plug_env = Dict.take(pdict, [:plug_env]) 
-      metadata = Dict.merge(plug_env, context) |> Enum.into(Map.new)
-
-      Honeybadger.notify exception, metadata, System.stacktrace
+      exception = exception_from_message(message)
+      context = Dict.drop(pdict, @ignored_keys) |> Enum.into(Map.new)
+      Honeybadger.notify exception, context, System.stacktrace
     rescue
       ex ->
         error_type = Utils.strip_elixir_prefix(ex.__struct__)
         message = "Unable to notify Honeybadger! #{error_type}: #{ex.message}"
-        Logger.error(message)
+        Logger.warn(message)
     end
 
-    {:ok, context_keys}
+    {:ok, state}
   end
 
   defp exception_from_message(message) do
