bfe2acf5b5d4457b7e2f134fb904fcec6e8c0584
Richard Bishop <richard@rubiquity.com>
2016-02-03 22:02:28 -0700

    Release v0.3.1

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Honeybadger.Mixfile do
 
   def project do
     [app: :honeybadger,
-     version: "0.3.0",
+     version: "0.3.1",
      elixir: "~> 1.0",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
