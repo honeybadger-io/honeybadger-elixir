9d81f4ec79be60b4b23d3f94e29e73fb229fd0b7
Richard Bishop <richard@rubiquity.com>
2016-02-22 10:37:14 -1000

    Bump to 0.4.0

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Honeybadger.Mixfile do
 
   def project do
     [app: :honeybadger,
-     version: "0.3.1",
+     version: "0.4.0",
      elixir: "~> 1.0",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
