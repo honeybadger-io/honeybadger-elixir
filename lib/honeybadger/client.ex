8983bb8cd1007c1a202329f2d2bf962953b95399
Callum Barratt <callum@barratt.me>
2016-11-18 09:31:06 +0000

    Quick first pass at proxy configuration for Honeybadger

--- lib/honeybadger/client.ex
+++ lib/honeybadger/client.ex
@@ -1,20 +1,29 @@
 defmodule Honeybadger.Client do
   alias Poison, as: JSON
 
-  defstruct [:environment_name, :headers, :hostname, :origin]
+  defstruct [:environment_name, :headers, :hostname, :origin, :proxy, :proxy_auth]
 
   @metrics_endpoint "/v1/metrics"
   @notices_endpoint "/v1/notices"
+
+  @headers [
+    {"Accept", "application/json"},
+    {"Content-Type", "application/json"}
+  ]
 
   def new do
     origin = Application.get_env(:honeybadger, :origin)
     api_key = Application.get_env(:honeybadger, :api_key)
     env_name = Application.get_env(:honeybadger, :environment_name)
     hostname = Application.get_env(:honeybadger, :hostname)
-    %__MODULE__{origin: origin, 
+    proxy = Application.get_env(:honeybadger, :proxy)
+    proxy_auth = Application.get_env(:honeybadger, :proxy_auth)
+    %__MODULE__{origin: origin,
                 headers: headers(api_key),
                 environment_name: env_name,
-                hostname: hostname}
+                hostname: hostname,
+                proxy: proxy,
+                proxy_auth: proxy_auth}
   end
 
   def send_metric(%__MODULE__{} = client, metric, http_mod \\ HTTPoison) do
@@ -24,23 +33,34 @@ defmodule Honeybadger.Client do
       metrics: metric
     })
 
-    http_mod.post(
-      client.origin <> @metrics_endpoint, body, client.headers
-    )
+    case client.proxy do
+      nil ->
+        http_mod.post(
+          client.origin <> @metrics_endpoint, body, client.headers
+        )
+      _ ->
+        http_mod.post(
+          client.origin <> @metrics_endpoint, body, client.headers, [proxy: client.proxy, proxy_auth: client.proxy_auth]
+        )
+    end
   end
 
   def send_notice(%__MODULE__{} = client, metric, http_mod \\ HTTPoison) do
     body = JSON.encode!(metric)
 
-    http_mod.post(
-      client.origin <> @notices_endpoint, body, client.headers
-    )
+    case client.proxy do
+      nil ->
+        http_mod.post(
+          client.origin <> @notices_endpoint, body, client.headers
+        )
+      _ ->
+        http_mod.post(
+          client.origin <> @notices_endpoint, body, client.headers, [proxy: client.proxy, proxy_auth: client.proxy_auth]
+        )
+    end
   end
 
   defp headers(api_key) do
-    [{"Accept", "application/json"},
-     {"Content-Type", "application/json"},
-     {"X-API-Key", api_key}]
+    @headers ++ [{"X-API-Key", api_key}]
   end
-
 end
