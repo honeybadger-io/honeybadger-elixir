f4d2142fe898931352cc22f2287b80b35fb90eb1
Micah Woods <micahwoods@gmail.com>
2017-02-27 13:09:46 -0500

    Remove metrics

--- lib/honeybadger/client.ex
+++ lib/honeybadger/client.ex
@@ -3,7 +3,6 @@ defmodule Honeybadger.Client do
 
   defstruct [:environment_name, :headers, :hostname, :origin]
 
-  @metrics_endpoint "/v1/metrics"
   @notices_endpoint "/v1/notices"
 
   def new do
@@ -11,29 +10,17 @@ defmodule Honeybadger.Client do
     api_key = Application.get_env(:honeybadger, :api_key)
     env_name = Application.get_env(:honeybadger, :environment_name)
     hostname = Application.get_env(:honeybadger, :hostname)
-    %__MODULE__{origin: origin, 
+    %__MODULE__{origin: origin,
                 headers: headers(api_key),
                 environment_name: env_name,
                 hostname: hostname}
   end
 
-  def send_metric(%__MODULE__{} = client, metric, http_mod \\ HTTPoison) do
-    body = JSON.encode!(%{
-      environment: client.environment_name,
-      hostname: client.hostname,
-      metrics: metric
-    })
+  def send_notice(%__MODULE__{} = client, notice, http_mod \\ HTTPoison) do
+    body = JSON.encode!(notice)
 
     http_mod.post(
-      client.origin <> @metrics_endpoint, body, client.headers
-    )
-  end
-
-  def send_notice(%__MODULE__{} = client, metric, http_mod \\ HTTPoison) do
-    body = JSON.encode!(metric)
-
-    http_mod.post(
-      client.origin <> @notices_endpoint, body, client.headers
+      client.origin <> @notices_endpoint, notice, client.headers
     )
   end
 
