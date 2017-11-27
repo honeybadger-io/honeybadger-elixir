67b716c322698003e8e2585616d6d3743d5741d7
Parker Selbert <parker@sorentwo.com>
2017-11-27 14:47:45 -0600

    Apply limits when inspecting arguments
    To prevent serializing entire structs or lengthy binaries we now pass
    opts to `Kernel.inspect/2` when generating a backtrace.

--- lib/honeybadger/backtrace.ex
+++ lib/honeybadger/backtrace.ex
@@ -3,35 +3,45 @@ defmodule Honeybadger.Backtrace do
   The Backtrace module contains functions for formatting system stacktraces.
   """
 
-  @app_context "app"
-  @all_context "all"
+  @inspect_opts charlists: :as_lists,
+                limit: 5,
+                printable_limit: 1024,
+                pretty: false
 
   @doc """
   Convert a system stacktrace into an API compatible backtrace.
 
-  The function expects a list of `:erlang.stack_item()` tuples.
+  When `filter_args` is disabled the arguments will be included. Arguments are
+  inspected and reported as binaries, regardless of the original format.
+
+      iex> stack_item = {:erlang, :funky, [{:ok, 123}], []}
+      ...> Honeybadger.Backtrace.from_stacktrace([stack_item])
+      [%{file: nil, number: nil, method: "funky/1", args: [], context: "all"}]
   """
+  @spec from_stacktrace(list(:erlang.stack_item())) :: list(map)
   def from_stacktrace(stacktrace) when is_list(stacktrace) do
     Enum.map(stacktrace, &format_line/1)
   end
 
   defp format_line({mod, fun, args, []}) do
-    format_line({mod, fun, args, [file: [], line: nil]})
+    format_line({mod, fun, args, [file: nil, line: nil]})
   end
 
   defp format_line({mod, fun, args, [file: file, line: line]}) do
     app = Honeybadger.get_env(:app)
     filter_args = Honeybadger.get_env(:filter_args)
 
-    %{file: format_file(file),
+    %{
+      file: format_file(file),
       method: format_method(fun, args),
       args: format_args(args, filter_args),
       number: line,
-      context: app_context(app, Application.get_application(mod))}
+      context: app_context(app, Application.get_application(mod))
+    }
   end
 
-  defp app_context(app, app) when not is_nil(app), do: @app_context
-  defp app_context(_app1, _app2), do: @all_context
+  defp app_context(app, app) when not is_nil(app), do: "app"
+  defp app_context(_app1, _app2), do: "all"
 
   defp format_file(""), do: nil
   defp format_file(file) when is_binary(file), do: file
@@ -40,14 +50,16 @@ defmodule Honeybadger.Backtrace do
   defp format_method(fun, args) when is_list(args) do
     format_method(fun, length(args))
   end
+
   defp format_method(fun, arity) when is_integer(arity) do
     "#{fun}/#{arity}"
   end
 
   defp format_args(args, false = _filter) when is_list(args) do
-    Enum.map(args, &inspect/1)
+    Enum.map(args, &Kernel.inspect(&1, @inspect_opts))
   end
-  defp format_args(_arity, _filter) do
+
+  defp format_args(_args_or_arity, _filter) do
     []
   end
 end
