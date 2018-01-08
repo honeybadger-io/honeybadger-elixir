ddd8cbc7e3c7526cc4b9a3f50679c5e490bdca65
Parker Selbert <parker@sorentwo.com>
2018-01-07 21:09:25 -0600

    Collapse plug data extraction into a single module
    This replaces reliance on Phoenix functions with pattern matching,
    eliminating the need to switch modules at compile time. Additionally,
    all metadata is extracted through a central `metadata` function, which
    can be supplied by users instead of overriding the `handle_errors`
    function.

--- lib/honeybadger/plug.ex
+++ lib/honeybadger/plug.ex
@@ -1,99 +1,70 @@
 if Code.ensure_loaded?(Plug) do
   defmodule Honeybadger.Plug do
-    alias Plug.Conn
-    alias Honeybadger.{PhoenixData, PlugData}
+    @moduledoc """
+    The `Honeybadger.Plug` adds automatic error handling to a plug pipeline.
+
+    Within a `Plug.Router` or `Phoenix.Router` use the module and crashes will
+    be reported to Honeybadger. It's best to `use Honeybadger.Plug` **after
+    the Router plugs** so that exceptions due to non-matching routes are not
+    reported to Honeybadger.
+
+    ### Example
+
+        defmodule MyPhoenixApp.Router do
+          use Crywolf.Web, :router
+          use Honeybadger.Plug
+
+          pipeline :browser do
+            [...]
+          end
+        end
+
+    ## Customizing
+
+    Data reporting may be customized by passing an alterate `:plug_data`
+    module. This is useful when working with alternate frameworks, such as
+    Absinthe for GraphQL APIs.
+
+    Any module with a `metadata/2` function that accepts a `Plug.Conn` and a
+    `module` name can be used to generate metadata.
+
+    ### Example
+
+        defmodule MyPhoenixApp.Router do
+          use Crywolf.Web, :router
+          use Honeybadger.Plug, plug_data: MyAbsinthePlugData
+        end
+    """
+
+    alias Honeybadger.PlugData
 
     @doc false
-    defmacro __using__(_opts) do
+    defmacro __using__(opts) do
       quote location: :keep do
         use Plug.ErrorHandler
 
-        @conn_data_mod if Code.ensure_loaded?(Phoenix), do: PhoenixData, else: PlugData
+        @plug_data Keyword.get(unquote(opts), :plug_data, PlugData)
 
-        def handle_errors(_conn, %{reason: %FunctionClauseError{function: :do_match}}), do: nil
+        @doc """
+        Called by `Plug.ErrorHandler` when an error is caught.
 
-        def handle_errors(_conn, %{reason: %{plug_status: 404}}), do: nil
+        By default this ignores "Not Found" errors for `Plug` or `Phoenix`
+        pipelines. It may be overridden to ignore additional errors or to
+        customize the data that is used for notifications.
+        """
+        @spec handle_errors(Plug.Conn.t(), %{kind: atom(), reason: any(), stack: any()}) :: :ok
+        def handle_errors(_conn, %{reason: %FunctionClauseError{function: :do_match}}), do: :ok
+
+        def handle_errors(_conn, %{reason: %{plug_status: 404}}), do: :ok
 
         def handle_errors(conn, %{reason: reason, stack: stack}) do
-          metadata = %{
-            plug_env: build_plug_env(conn, __MODULE__),
-            context: build_context(conn, __MODULE__)
-          }
+          metadata = @plug_data.metadata(conn, __MODULE__)
 
           Honeybadger.notify(reason, metadata, stack)
         end
 
-        def build_plug_env(conn, module) do
-          Honeybadger.Plug.build_plug_env(conn, module, @conn_data_mod)
-        end
-
-        def build_cgi_data(conn, _module) do
-          Honeybadger.Plug.build_cgi_data(conn)
-        end
-
-        def build_context(conn, _module) do
-          Honeybadger.context()
-        end
-
-        defoverridable build_cgi_data: 2,
-                       build_plug_env: 2,
-                       build_context: 2,
-                       handle_errors: 2
+        defoverridable handle_errors: 2
       end
     end
-
-    def build_plug_env(%Conn{} = conn, module, conn_data_mod) do
-      conn = Conn.fetch_query_params(conn)
-
-      %{
-        action: conn_data_mod.action(conn),
-        cgi_data: build_cgi_data(conn),
-        component: conn_data_mod.component(conn, module),
-        params: conn.params,
-        session: %{},
-        url: conn.request_path
-      }
-    end
-
-    def build_cgi_data(%Conn{} = conn) do
-      cgi_data = %{
-        "REQUEST_METHOD" => conn.method,
-        "PATH_INFO" => Enum.join(conn.path_info, "/"),
-        "QUERY_STRING" => conn.query_string,
-        "SCRIPT_NAME" => Enum.join(conn.script_name, "/"),
-        "REMOTE_ADDR" => get_remote_addr(conn),
-        "REMOTE_PORT" => get_remote_port(conn),
-        "SERVER_ADDR" => "127.0.0.1",
-        "SERVER_NAME" => Honeybadger.get_env(:hostname),
-        "SERVER_PORT" => conn.port,
-        "CONTENT_LENGTH" => Conn.get_req_header(conn, "content-length"),
-        "ORIGINAL_FULLPATH" => conn.request_path
-      }
-
-      conn
-      |> get_rack_format_headers()
-      |> Map.merge(cgi_data)
-    end
-
-    # Helpers
-
-    @doc false
-    def get_rack_format_headers(%Conn{req_headers: req_headers}) do
-      Enum.reduce(req_headers, %{}, fn {header, value}, acc ->
-        header = ("HTTP_" <> String.upcase(header)) |> String.replace("-", "_")
-
-        Map.put(acc, header, value)
-      end)
-    end
-
-    @doc false
-    def get_remote_addr(%Conn{remote_ip: remote_ip}) do
-      remote_ip
-      |> :inet.ntoa()
-      |> List.to_string()
-    end
-
-    @doc false
-    def get_remote_port(%Conn{peer: {_, port}}), do: port
   end
 end
--- test/honeybadger/plug_test.exs
+++ test/honeybadger/plug_test.exs
@@ -6,50 +6,12 @@ defmodule Honeybadger.PlugTest do
     use Plug.Router
     use Honeybadger.Plug
 
-    import Plug.Conn
-
     plug(:match)
     plug(:dispatch)
 
     get "/bang" do
       _ = conn
       raise RuntimeError, "Oops"
-    end
-  end
-
-  describe "build_plug_env/3" do
-    test "building outside of a phoenix app" do
-      conn = conn(:get, "/bang?foo=bar")
-
-      plug_env = %{
-        action: "",
-        cgi_data: Honeybadger.Plug.build_cgi_data(conn),
-        component: "Honeybadger.PlugTest.PlugApp",
-        params: %{"foo" => "bar"},
-        session: %{},
-        url: "/bang"
-      }
-
-      assert plug_env == Honeybadger.Plug.build_plug_env(conn, PlugApp, Honeybadger.PlugData)
-    end
-
-    test "building inside of a phoenix app" do
-      conn =
-        :get
-        |> conn("/bang?foo=bar")
-        |> put_private(:phoenix_controller, DanController)
-        |> put_private(:phoenix_action, :fight)
-
-      plug_env = %{
-        action: "fight",
-        cgi_data: Honeybadger.Plug.build_cgi_data(conn),
-        component: "DanController",
-        params: %{"foo" => "bar"},
-        session: %{},
-        url: "/bang"
-      }
-
-      assert plug_env == Honeybadger.Plug.build_plug_env(conn, PlugApp, Honeybadger.PhoenixData)
     end
   end
 
@@ -77,39 +39,5 @@ defmodule Honeybadger.PlugTest do
 
       refute_receive {:api_request, _}
     end
-  end
-
-  test "build_cgi_data/1" do
-    conn = conn(:get, "/bang")
-    {_, remote_port} = conn.peer
-
-    cgi_data = %{
-      "CONTENT_LENGTH" => [],
-      "ORIGINAL_FULLPATH" => "/bang",
-      "PATH_INFO" => "bang",
-      "QUERY_STRING" => "",
-      "REMOTE_ADDR" => "127.0.0.1",
-      "REMOTE_PORT" => remote_port,
-      "REQUEST_METHOD" => "GET",
-      "SCRIPT_NAME" => "",
-      "SERVER_ADDR" => "127.0.0.1",
-      "SERVER_NAME" => Application.get_env(:honeybadger, :hostname),
-      "SERVER_PORT" => 80
-    }
-
-    assert cgi_data == Honeybadger.Plug.build_cgi_data(conn)
-  end
-
-  test "get_remote_addr/1" do
-    conn = %Plug.Conn{remote_ip: {127, 0, 0, 1}}
-
-    assert "127.0.0.1" == Honeybadger.Plug.get_remote_addr(conn)
-  end
-
-  test "get_rack_format_headers/1" do
-    conn = %Plug.Conn{req_headers: [{"content-type", "application/json"}]}
-    rack_format = %{"HTTP_CONTENT_TYPE" => "application/json"}
-
-    assert rack_format == Honeybadger.Plug.get_rack_format_headers(conn)
   end
 end
