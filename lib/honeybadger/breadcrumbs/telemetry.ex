306d0627f2f2e082ad236bf2b29cb49346cb6b60
Kevin Webster <kevin@kevinwebster.me>
2019-09-04 10:27:58 -0700

    Fixed diaylzer issue

--- lib/honeybadger/breadcrumbs/telemetry.ex
+++ lib/honeybadger/breadcrumbs/telemetry.ex
@@ -8,10 +8,14 @@ defmodule Honeybadger.Breadcrumbs.Telemetry do
     )
   end
 
-  def handle_telemetry([:phoenix, :router_dispatch, :start], _, meta, _) do
+  def handle_telemetry([:phoenix, :router_dispatch, :start], _, meta, data) do
+    metadata =
+      Map.take(meta, [:plug, :plug_opts, :route, :pipe_through])
+      |> Map.update(:pipe_through, "", &inspect/1)
+
     Honeybadger.add_breadcrumb("Phoenix Router Dispatch",
-      metadata: Map.take(meta, [:plug, :plug_opts, :route, :pipe_through]),
-      category: :request
+      metadata: metadata,
+      category: "request"
     )
   end
 end
