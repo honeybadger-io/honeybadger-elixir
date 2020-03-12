3ec80e4adfb0dc91653fa8d2318d288cd25b698e
Kevin Webster <kevin@kevinwebster.me>
2020-03-12 12:59:51 -0700

    Release v0.14.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,8 @@ CHANGELOG](http://keepachangelog.com/) for how to update this file. This project
 adheres to [Semantic Versioning](http://semver.org/).
 
 ## [Unreleased]
+
+## [v0.14.0] - 2020-03-12
 ### Changed
 - Breadcrumbs on by default
 
--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Honeybadger.Mixfile do
   def project do
     [
       app: :honeybadger,
-      version: "0.13.1",
+      version: "0.14.0",
       elixir: "~> 1.7",
       consolidate_protocols: Mix.env() != :test,
       build_embedded: Mix.env() == :prod,
