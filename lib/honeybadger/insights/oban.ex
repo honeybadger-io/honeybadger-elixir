c038d239168c37494a2b4a75fa9695a295e3344e
Kevin Webster <kevin@kevinwebster.me>
2025-03-17 16:55:05 -0700

    Add helper for Oban & extract request_id from job

--- lib/honeybadger/insights/oban.ex
+++ lib/honeybadger/insights/oban.ex
@@ -29,14 +29,42 @@ defmodule Honeybadger.Insights.Oban do
   @required_dependencies [Oban]
 
   @telemetry_events [
+    [:oban, :job, :start],
     [:oban, :job, :stop],
     [:oban, :job, :exception]
   ]
 
+  ## Public API
+
+  @doc """
+  Adds the current Honeybadger request ID to the job's metadata.
+
+  ## Example
+
+      MyApp.Worker.new()
+      |> Honeybadger.Insights.Oban.add_request_id()
+      |> Oban.insert()
+  """
+  def add_request_id(%Ecto.Changeset{} = changeset) do
+    meta = Ecto.Changeset.get_field(changeset, :meta) || %{}
+    updated_meta = Map.put(meta, "hb_request_id", Honeybadger.get_request_id())
+    Ecto.Changeset.change(changeset, meta: updated_meta)
+  end
+
+  ## Overridable Telemetry Handlers (Internal)
+
+  @doc false
   def extract_metadata(%{conf: conf, job: job, state: state}, _event) do
     job
     |> Map.take(~w(args attempt id queue tags worker)a)
     |> Map.put(:prefix, conf.prefix)
     |> Map.put(:state, state)
+  end
+
+  @doc false
+  def handle_telemetry([:oban, :job, :start], _measurements, %{job: job}, _config) do
+    if request_id = job.meta["hb_request_id"] do
+      Honeybadger.set_request_id(request_id)
+    end
   end
 end
