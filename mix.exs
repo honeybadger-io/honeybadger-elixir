8a8ffa676d22ce687b8cf6062aab47f200ff79f0
Pangratios Cosma <pangratios.cosma@honeybadger.io>
2025-11-05 09:24:29 +0200

    chore: remove expublish from deps

--- mix.exs
+++ mix.exs
@@ -113,7 +113,6 @@ defmodule Honeybadger.Mixfile do
       # Dev dependencies
       {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
       {:dialyxir, "~> 1.0", only: :dev, runtime: false},
-      {:expublish, "~> 2.5", only: [:dev], runtime: false},
 
       # Test dependencies
       {:plug_cowboy, ">= 2.0.0 and < 3.0.0", only: :test},
